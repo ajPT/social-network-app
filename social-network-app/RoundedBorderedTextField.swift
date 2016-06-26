a2d9972080876cd98921c71604fc294a4bbfbeca
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-06-26 16:36:31 +0100

    *wip* Login Screen

---
+++ social-network-app/RoundedBorderedTextField.swift
@@ -0,0 +1,19 @@
+//
+//  BorderedTextField.swift
+//  social-network-app
+//
+//  Created by Amadeu Andrade on 26/06/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import UIKit
+
+class RoundedBorderedTextField: UITextField {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+    
+}
