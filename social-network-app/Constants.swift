ed68665281b527ad6bba409ffc07428deac1dc8a
Amadeu Andrade <amadeuandrade@Amadeus-MBP.home>
2016-06-26 15:55:35 +0100

    *wip* login screen

---
+++ social-network-app/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  social-network-app
+//
+//  Created by Amadeu Andrade on 26/06/16.
+//  Copyright © 2016 Amadeu Andrade. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
