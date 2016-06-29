876e1e32b53497320bbd45d7b6cf54476225ac8c
Amadeu Andrade <amadeuandrade@Amadeus-MacBook-Pro.local>
2016-06-29 19:01:23 +0100

    *wip* Authenticate with Firebase[A

--- social-network-app/DataService.swift
+++ social-network-app/DataService.swift
@@ -11,12 +11,12 @@ import Firebase
 
 class DataService {
 
-    static let ds = DataService()
-    
-    private var _REF_BASE = FIRDatabase.database().reference()
-    
-    var REF_BASE: FIRDatabaseReference {
-        return _REF_BASE
-    }
+//    static let ds = DataService()
+//    
+//    private var _REF_BASE = FIRDatabase.database().reference()
+//    
+//    var REF_BASE: FIRDatabaseReference {
+//        return _REF_BASE
+//    }
     
 }
