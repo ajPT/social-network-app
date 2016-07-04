01234fb341e6de409a6c7f1d06adc4a38e504343
Amadeu Andrade <amadeuandrade@Amadeus-MBP.lan>
2016-07-04 15:19:50 +0100

    Creating users in firebase (Firebase)

--- social-network-app/SignUpVC.swift
+++ social-network-app/SignUpVC.swift
@@ -32,32 +32,23 @@ class SignUpVC: UIViewController {
         if let email = emailField.text where email != "", let password = passwordField.text where password != "" {
             
             FIRAuth.auth()?.createUserWithEmail(email, password: password) { (user, error) in
-                if error != nil {
-                    if error?.code == STATUS_ERROR_EMAIL_ALREADY_IN_USE {
+                if let err = error {
+                    if err.code == STATUS_ERROR_EMAIL_ALREADY_IN_USE {
                         UtilAlerts().showAlert(self, title: UtilAlerts.Titles.ERROR_EMAIL_ALREADY_IN_USE, msg: UtilAlerts.GeneralMessages.ERROR_EMAIL_ALREADY_IN_USE)
-                    } else if error?.code == STATUS_ERROR_WEAK_PASSWORD {
+                    } else if err.code == STATUS_ERROR_WEAK_PASSWORD {
                         self.passwordField.text = ""
                         UtilAlerts().showAlert(self, title: UtilAlerts.Titles.ERROR_WEAK_PASSWORD, msg: UtilAlerts.GeneralMessages.ERROR_WEAK_PASSWORD)
-                    } else if error?.code == STATUS_ERROR_NETWORK_REQUEST_FAILED {
+                    } else if err.code == STATUS_ERROR_NETWORK_REQUEST_FAILED {
                         UtilAlerts().showAlert(self, title: UtilAlerts.Titles.ERROR_NETWORK_REQUEST_FAILED, msg: UtilAlerts.NetworkMessages.ERROR_NETWORK_REQUEST_FAILED)
                     } else {
-                        print(error)
+                        print(err)
                         UtilAlerts().showAlert(self, title: UtilAlerts.Titles.UNKNOWN, msg: UtilAlerts.CreateAccountMessages.UNKNOWN_ERROR_CREATE)
                     }
                 } else if let userr = user {
-                    let provider = userr.providerID
-                    let uid = userr.uid
-                    if provider != "" && uid != "" {
-                        DataService.ds.createFirebaseUser(uid, userInfo: ["provider":provider])
-                        NSUserDefaults.standardUserDefaults().setValue(uid, forKey: KEY_UID)
-                        FIRAuth.auth()?.signInWithEmail(email, password: password, completion: { (_: FIRUser?, err: NSError?) in
-                            if err != nil {
-                                print(err)
-                                UtilAlerts().showAlert(self, title: UtilAlerts.Titles.UNKNOWN, msg: UtilAlerts.CreateAccountMessages.UNKNOWN_ERROR_CREATE_LOGIN)
-                            } else {
-                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
-                            }
-                            })
+                    if userr.providerID != "" && userr.uid != "" {
+                        DataService.ds.createFirebaseUser(userr.uid, userInfo: ["provider":userr.providerID])
+                        NSUserDefaults.standardUserDefaults().setValue(userr.uid, forKey: KEY_UID)
+                        self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                     } else {
                         UtilAlerts().showAlert(self, title: UtilAlerts.Titles.UNKNOWN, msg: UtilAlerts.GeneralMessages.UNKNOWN)
                     }
@@ -71,6 +62,7 @@ class SignUpVC: UIViewController {
        
     }
 
+    
     //MARK: Cancel
     @IBAction func onCancelBtnPressed(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
