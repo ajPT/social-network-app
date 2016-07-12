1da32731abdb30819df461b74dd1984845781fd6
Amadeu Andrade <amadeuandrade@Amadeus-MacBook-Pro.local>
2016-07-12 23:11:03 +0100

    *WIP* profile VC layout

--- social-network-app/ProfileVC.swift
+++ social-network-app/ProfileVC.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ProfileVC: UIViewController {
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var userImg: RoundedImage!
+    @IBOutlet weak var usernameField: RoundedBorderedTextField!
+    @IBOutlet weak var emailField: RoundedBorderedTextField!
+    @IBOutlet weak var oldPassField: RoundedBorderedTextField!
+    @IBOutlet weak var newPassField: RoundedBorderedTextField!
+    
+    
     //MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    //MARK: - IBActions
+    
+    @IBAction func onChangePicPressed(sender: UIButton) {
+    }
+    
+    @IBAction func onSaveBtnPressed(sender: UIButton) {
     }
 
 
